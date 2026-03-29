cf40d5e68689fa84892ac2ba80c7d128f50baa71
mmartiss <martynasabrai@gmail.com>
2026-03-29 20:29:35 +0300

    Changed server.R and ui.R so other modules would show up properly. Currently all modules except upload are empty.

--- server.R
+++ server.R
@@ -1,40 +1,63 @@
-# ============================================================
-#  server.R
-# ============================================================
+# server.R
 
 server <- function(input, output, session) {
-
+  
   rv <- reactiveValues(
     page  = "upload",
-    steps = c(upload = FALSE)
+    # Track which steps are complete
+    steps = c(upload = FALSE, qc = FALSE, filter = FALSE, annot = FALSE, viz = FALSE)
   )
-
-  observeEvent(input$nav_upload, { rv$page <- "upload" })
-
+  
+  # --- Navigation Logic ---
+  # This observer switches classes to show/hide pages when buttons are clicked
+  observe({
+    req(input$nav_upload, input$nav_qc, input$nav_filter, input$nav_annot, input$nav_viz)
+    
+    pages <- c("upload", "qc", "filter", "annot", "viz")
+    
+    lapply(pages, function(p) {
+      observeEvent(input[[paste0("nav_", p)]], {
+        rv$page <- p
+        # Use shinyjs to toggle visibility via the "active" class defined in your CSS
+        for (pg in pages) {
+          if (pg == p) {
+            shinyjs::addClass(selector = paste0("#pg_", pg), class = "active")
+            shinyjs::addClass(id = paste0("nav_", pg), class = "active")
+          } else {
+            shinyjs::removeClass(selector = paste0("#pg_", pg), class = "active")
+            shinyjs::removeClass(id = paste0("nav_", pg), class = "active")
+          }
+        }
+      })
+    })
+  })
+  
   output$strip_title <- renderText({ rv$page })
 
+  upload_out <- mod_upload_server("upload")
+
+  qc_out <- mod_qc_server("qc", data = upload_out$data)
+
+  filter_out <- mod_filter_server("filter", data = upload_out$data)
+
+  annot_out <- mod_annotation_server("annot", data = filter_out$data)
+
+  mod_visualization_server("viz", data = filter_out$data)
+  
+  observe({
+    req(upload_out$data())
+    rv$steps["upload"] <- TRUE
+  })
+  
   output$step_status <- renderUI({
     done  <- sum(rv$steps)
     total <- length(rv$steps)
     div(class = "step-counter", sprintf("%d / %d", done, total))
   })
-
+  
   output$pipeline_dots <- renderUI({
     tagList(lapply(names(rv$steps), function(s) {
-      div(class = paste("dot", if (isTRUE(rv$steps[[s]])) "done"),
-          title = s)
+      div(class = paste("dot", if (isTRUE(rv$steps[[s]])) "done"), title = s)
     }))
-  })
-
-  # Upload module
-  upload_out <- mod_upload_server("upload")
-  #upload_out$data          : reactive data.frame
-  #upload_out$filename      : reactive character
-  #upload_out$analysis_type : reactive character
-  #upload_out$sep           : reactive character
-
-  observe({
-    req(upload_out$data())
-    rv$steps["upload"] <- TRUE
   })
 }
--- ui.R
+++ ui.R
@@ -19,34 +19,42 @@ ui <- tagList(
 
   div(class = "shell",
 
-    tags$nav(class = "rail",
-      div(class = "rail-wordmark", "variantR"),
-      tags$ul(class = "rail-nav",
-        rail_item("nav_upload", "01", "upload", active = TRUE)
+      tags$nav(class = "rail",
+               div(class = "rail-wordmark", "variantR"),
+               tags$ul(class = "rail-nav",
+                       # ADDED THE OTHER NAV ITEMS HERE
+                       rail_item("nav_upload", "01", "upload", active = TRUE),
+                       rail_item("nav_qc",     "02", "quality control"),
+                       rail_item("nav_filter", "03", "filtering"),
+                       rail_item("nav_annot",  "04", "annotation"),
+                       rail_item("nav_viz",    "05", "visualization")
+               ),
+               div(class = "rail-bottom",
+                   div(class = "pipe-dots", uiOutput("pipeline_dots")),
+                   div(class = "rail-ver", "v0.1.0")
+               )
       ),
-      div(class = "rail-bottom",
-        div(class = "pipe-dots", uiOutput("pipeline_dots")),
-        div(class = "rail-ver", "v0.1.0")
-      )
-    ),
-
-    div(class = "content",
-      div(class = "strip",
-        div(class = "strip-path",
-          tags$span(class = "strip-root", "files"),
-          tags$span(class = "strip-arrow", "\u203a"),
-          textOutput("strip_title", inline = TRUE)
-        ),
-        div(class = "strip-right",
-          uiOutput("step_status"),
-          actionButton("btn_run", "run", class = "run-btn")
-        )
-      ),
-      div(class = "pages",
-        div(id = "pg_upload", class = "pg active",
-          mod_upload_ui("upload")
-        )
+      
+      div(class = "content",
+          div(class = "strip",
+              div(class = "strip-path",
+                  tags$span(class = "strip-root", "files"),
+                  tags$span(class = "strip-arrow", "\u203a"),
+                  textOutput("strip_title", inline = TRUE)
+              ),
+              div(class = "strip-right",
+                  uiOutput("step_status"),
+                  actionButton("btn_run", "run", class = "run-btn")
+              )
+          ),
+          div(class = "pages",
+              # ADDED THE OTHER PAGE DIVS HERE
+              div(id = "pg_upload", class = "pg active", mod_upload_ui("upload")),
+              div(id = "pg_qc",     class = "pg",        mod_qc_ui("qc")),
+              div(id = "pg_filter", class = "pg",        mod_filter_ui("filter")),
+              div(id = "pg_annot",  class = "pg",        mod_annotation_ui("annot")),
+              div(id = "pg_viz",    class = "pg",        mod_visualization_ui("viz"))
+          )
       )
     )
-  )
 )
